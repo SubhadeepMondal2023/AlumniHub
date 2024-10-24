b62225f6aa101fdcc8677ad0217c09f52bfb1dcd
IshaniSen2612 <128341099+IshaniSen2612@users.noreply.github.com>
2024-10-24 21:37:32 +0530

    Server initiated

---
+++ server/src/main/java/com/alumnihub/AlumniHub/AlumniHubApplication.java
@@ -0,0 +1,13 @@
+package com.alumnihub.AlumniHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniHubApplication.class, args);
+	}
+
+}
