33583b4e43804d927eecc4716aeb976ea38bbeb5
Bhargav-Techin <148316999+Bhargav-Techin@users.noreply.github.com>
2024-11-06 22:55:53 +0530

    Stop tracking application.properties

--- server/src/main/java/com/alumnihub/AlumniHub/model/UserModel.java
+++ server/src/main/java/com/alumnihub/AlumniHub/model/UserModel.java
@@ -1,33 +1,27 @@
 package com.alumnihub.AlumniHub.model;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Document(collection = "users")
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
 
     @Id
-    private ObjectId id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
-    private int year=-1;
-    public ObjectId getId() {
-        return id;
-    }
-    public void setId(ObjectId id) {
-        this.id=id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name=name;
-    }
-    public int getYear() {
-        return year;
-    }
-    public void setYear(int year) {
-        this.year=year;
-    }   
-        
+    private int year = -1;
 }
