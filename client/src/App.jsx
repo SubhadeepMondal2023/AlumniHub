fddf5c9f105bcc05e71be39575c07d4f75bc59b0
Debasree <ndebasree382@gmail.com>
2024-11-10 19:21:50 +0530

    About Us implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroPage from './components/HeroSection/HeroPage.jsx';
+import AboutUs from './components/AboutUs/AboutUs.jsx'; // Import AboutUs component
 import Footer from './components/Footer/Footer.jsx';
 
-
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HeroPage />} />
+          <Route path="/about" element={<AboutUs />} /> {/* New AboutUs route */}
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
