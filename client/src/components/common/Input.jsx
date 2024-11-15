4205494ae37e4af1f67482af4e95de58fcd3ddc0
deepcu-it <deepghosal445@gmail.com>
2024-11-16 02:30:52 +0530

    input modified

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../css/input.css';
 
-const Input = ({type, placeholder, name, value, onChange, required}) => {
+const Input = ({type, placeholder, name, value, onChange, Component}) => {
   return (
     <div class="inputGroup">
       <input type={type} placeholder={placeholder} name={name} value={value} onChange={onChange} autocomplete="off" />
       <label for={name}>{name}</label>
+      {Component}
     </div>
   )
 }
