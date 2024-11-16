a1fcc045c26c9aca2b04f9e5fea3c6b8297e3439
Bhargav-Techin <148316999+Bhargav-Techin@users.noreply.github.com>
2024-11-16 17:11:51 +0530

    REFACTOR USER CONTROLLER add jwt authentication

--- server/src/main/java/com/alumnihub/AlumniHub/repository/UserRepository.java
+++ server/src/main/java/com/alumnihub/AlumniHub/repository/UserRepository.java
@@ -4,11 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.alumnihub.AlumniHub.model.Role;
 import com.alumnihub.AlumniHub.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     // Add custom query methods if needed
     Optional<User> findById(Long userId);
 
-    Optional<User> findByEmail(String email);
+    Optional<User>  findByEmail(String email);
+
+    User findByEmailAndRole(String email, Role role);
 }
