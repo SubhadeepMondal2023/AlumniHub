9bbcaea2758f0e39a81058213efa9396f9aa82ee
Debasree <ndebasree382@gmail.com>
2024-11-20 19:35:35 +0530

    file structure modified for rtk query

--- client/src/redux/actions/alumniActions.js
+++
@@ -1,8 +0,0 @@
-import { getAlumni } from "../../services/apiService";
-
-export const FETCH_ALUMNI_SUCCESS = "FETCH_ALUMNI_SUCCESS";
-
-export const fetchAlumni = (filters) => async (dispatch) => {
-  const data = await getAlumni(filters);
-  dispatch({ type: FETCH_ALUMNI_SUCCESS, payload: data });
-};
