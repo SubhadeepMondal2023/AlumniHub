0d12e32cdc49be590f54a9f47feb1e541cbc45e9
deepcu-it <deepghosal445@gmail.com>
2024-12-25 21:53:02 +0530

    login implemented

--- client/src/redux/api/alumniApiSlice.js
+++ client/src/redux/api/alumniApiSlice.js
@@ -1,20 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { testAlumniData } from "../../utils/Links";
 
+const baseQueryWithAuth = fetchBaseQuery({
+  baseUrl: 'http://localhost:8080/api',
+  prepareHeaders: (headers, { url }) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+      
+    }
+    return headers;
+  },
+});
+
 export const alumniApi = createApi({
   reducerPath: "alumniApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api" }),
+  baseQuery: baseQueryWithAuth,
   tagTypes: ["Alumni"],
   endpoints: (builder) => ({
     fetchAlumni: builder.query({
-      query: () => "/alumni",
-      providesTags: (result) =>
-        result
-          ? [
-              ...result.map(({ AlumniID }) => ({ type: "Alumni", id: AlumniID })),
-              { type: "Alumni", id: "LIST" },
-            ]
-          : [{ type: "Alumni", id: "LIST" }],
+      query: ({designation,location,yoe,degree,currentCompany,searchByName}) => {
+        return `/alumni?designation=${designation}&location=${location}&yoe=${yoe}&degree=${degree}&currentCompany=${currentCompany}&searchByName=${searchByName}`
+      },
     }),
     deleteAlumni: builder.mutation({
       query: (alumniId) => ({
