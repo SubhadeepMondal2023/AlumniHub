b879bdec782554e4ede817003cf73d8f371896cb
deepcu-it <deepghosal445@gmail.com>
2025-01-01 21:15:36 +0530

    jwt exipy increased

--- server/src/main/java/com/alumnihub/AlumniHub/jwt/JwtProvider.java
+++ server/src/main/java/com/alumnihub/AlumniHub/jwt/JwtProvider.java
@@ -22,7 +22,7 @@ public class JwtProvider {
                 .setSubject(email) // Set email as the subject
                 .claim("email", email) // Add email claim
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour validity
+                .setExpiration(new Date(System.currentTimeMillis() + 7*24*3600000)) // 1 hour validity
                 .signWith(key, SignatureAlgorithm.HS256) // Use the consistent signing key
                 .compact();
     }
